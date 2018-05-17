d06a6083d1e7f29d0e42f0b2ee008f02cee844f9
Hasse Nielsen <hasse.nielsen@gmail.com>
2018-05-17 13:07:39 +0200

    adding wifiConfig changes

--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ namespace MBW {
      * @param ssid The wifi we connect to
      * @param password The password for the wifi
      */
-    //% weight=10 blockId="wifiConfig" block="wifiConfig"
+    //% weight=10 blockId="wifiConfig" block="Connect to this network %ssid| with this password %password |%"
     export function wifiConfig(ssid: "SSID", password: "PASSWORD"): void {
 
     }
