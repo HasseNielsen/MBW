3de3848add7b080f42c7cc4ab876b8a1f975bf5e
Hasse Nielsen <hasse.nielsen@gmail.com>
2018-05-17 12:33:18 +0200

    Changes ...

--- main.ts
+++ main.ts
@@ -3,13 +3,13 @@
 /**
  * Blocks for MBW
  */
-//% color=#494a4e weight=10 icon="\uf1ae"
+//% color=#494a4e weight=10 icon="\uF0EB"
 namespace MBW {
 
     /**
      * Starts a basic test, typically placed in "On Start"
      */
-    //% weight=10 blockId="startTEST" block="Start test"
+    //% weight=10 blockId="startMBW" block="Start test"
     export function startMBW(): void {
 
     }
