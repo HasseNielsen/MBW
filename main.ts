5a4dd1fc413b358c6804bdd9c8cb8c15919ec2c8
Hasse Lykke Nielsen <hasse.nielsen@gmail.com>
2018-07-14 20:18:48 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,25 @@
  */
 //% color=#494a4e weight=10 icon="\uF0EB"
 namespace MBW {
+    /**
+    * Set pin RX and TX for ESP8266 Serial Wifi Module，Baud rate: 9600.
+    * @param wifiRX describe parameter here, eg: SerialPin.P12
+    * @param wifiTX describe parameter here, eg: SerialPin.P13
+    */
+    //% weight=100
+    //% blockId="wifi_init" block="set ESP8266 RX %wifiRX| TX %wifiTX|at baud rate 9600"
+    export function initwifi(wifiRX: SerialPin, wifiTX: SerialPin): void {
+        // Add code here
+        basic.pause(1000)
+        serial.redirect(
+            wifiRX,
+            wifiTX,
+            BaudRate.BaudRate9600
+        )
+        basic.pause(1000)
+        serial.writeLine("" + "\u000D" + "\u000A")
+        basic.pause(1000)
+    }
 
     /**
      * Wifi configuration, connect to ssid with password
@@ -16,58 +35,23 @@ namespace MBW {
         serial.writeLine("WIFI " + ssid + " " + password + "\u000D" + "\u000A")
         basic.pause(15000)
     }
-    
+
     /**
-     * Set pin RX and TX for ESP8266 Serial Wifi Module，Baud rate: 9600.
-     * @param wifiRX describe parameter here, eg: SerialPin.P12
-     * @param wifiTX describe parameter here, eg: SerialPin.P13
-     */
-    //% weight=100
-    //% blockId="wifi_init" block="set ESP8266 RX %wifiRX| TX %wifiTX|at baud rate 9600"
-    export function initwifi(wifiRX: SerialPin, wifiTX: SerialPin): void {
-        // Add code here
-        basic.pause(1000)
-        serial.redirect(
-            wifiRX,
-            wifiTX,
-            BaudRate.BaudRate9600
-        )       
-        basic.pause(1000)
-        serial.writeLine("" +  "\u000D" +  "\u000A")
-        basic.pause(1000)
-    }
-    
-     /**
-     * TODO: connectwifi，Fill in your ssid and your key.
-     * @param ssid describe parameter here, eg: "your ssid"
-     * @param key describe parameter here, eg: "your key"
-     */
-    //% weight=99
-    //% blockId="wifi_connect" block="connect to wifi SSDI: %ssid| KEY: %key"
-    export function connectwifi(ssid: string, key: string): void {
-        // Add code here
-    }
-    
-    /**
-     * TODO: Set data to be sent.
-     * @param write_api_key describe parameter here, eg: "your write api key"
-     * @param n1 describe parameter here, eg: 0
-     * @param n2 describe parameter here, eg: 0
-     * @param n3 describe parameter here, eg: 0
-     * @param n4 describe parameter here, eg: 0
-     * @param n5 describe parameter here, eg: 0
-     * @param n6 describe parameter here, eg: 0
-     * @param n7 describe parameter here, eg: 0
-     * @param n8 describe parameter here, eg: 0
-     */
+ * ThingSpeak: Set data to be sent.
+ * @param write_api_key describe parameter here, eg: "your write api key"
+ * @param n1 describe parameter here, eg: 0
+ * @param n2 describe parameter here, eg: 0
+ * @param n3 describe parameter here, eg: 0
+ * @param n4 describe parameter here, eg: 0
+ */
     //% weight=97
-    //% blockId="send_text" block="set data to be send : Write API Key= %write_api_key|field1= %n1|field2= %n2|field3= %n3|field4= %n4|field5= %n5|field6= %n6|field7= %n7|field8= %n8"
+    //% blockId="send_text" block="set data to be send : Write API Key= %write_api_key|field1= %n1|field2= %n2|field3= %n3|field4= %n48"
     export function tosendtext(write_api_key: string,
-                                n1: number, 
-                                n2: number, 
-                                n3: number, 
-                                n4: number): void {
-        let text=""   
+        n1: number,
+        n2: number,
+        n3: number,
+        n4: number): void {
+        let text = ""
         text = "GET /update?key="
             + write_api_key
             + " "
@@ -79,7 +63,7 @@ namespace MBW {
             + " "
             + n4
         serial.writeString(text + "\u000D" + "\u000A")
-        basic.pause(8000)             
+        basic.pause(8000)
         // Add code here
     }
 }
