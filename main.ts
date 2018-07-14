d4db4f4fead798676d0b80b871412c7a14559a7a
Hasse Lykke Nielsen <hasse.nielsen@gmail.com>
2018-07-14 20:33:42 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,12 +6,12 @@
 //% color=#494a4e weight=10 icon="\uF0EB"
 namespace MBW {
     /**
-    * Set pin RX and TX for ESP8266 Serial Wifi Module，Baud rate: 9600.
+    * Set pin RX and TX for the wifi module.
     * @param wifiRX describe parameter here, eg: SerialPin.P12
     * @param wifiTX describe parameter here, eg: SerialPin.P13
     */
     //% weight=100
-    //% blockId="wifi_init" block="set ESP8266 RX %wifiRX| TX %wifiTX|at baud rate 9600"
+    //% blockId="wifi_init" block="Wifi config:\n Set wifi RX %wifiRX| TX %wifiTX|"
     export function initwifi(wifiRX: SerialPin, wifiTX: SerialPin): void {
         // Add code here
         basic.pause(1000)
@@ -30,7 +30,7 @@ namespace MBW {
      * @param ssid The wifi we connect to, eg: "Your SSID"
      * @param password The password for the wifi, eg: "Your password"
      */
-    //% weight=10 blockId="wifiConfig" block="Connect to this network: %ssid| with this password: %password"
+    //% weight=10 blockId="wifiConfig" block="Wifi config: Connect to this network: %ssid| with this password: %password"
     export function wifiConfig(ssid: string, password: string): void {
         serial.writeLine("WIFI " + ssid + " " + password + "\u000D" + "\u000A")
         basic.pause(15000)
@@ -45,7 +45,7 @@ namespace MBW {
  * @param n4 describe parameter here, eg: 0
  */
     //% weight=97
-    //% blockId="send_text" block="set data to be send : Write API Key= %write_api_key|field1= %n1|field2= %n2|field3= %n3|field4= %n48"
+    //% blockId="send_text" block="ThingSpeak: Set data to be send : Write API Key= %write_api_key|field1= %n1|field2= %n2|field3= %n3|field4= %n48"
     export function tosendtext(write_api_key: string,
         n1: number,
         n2: number,
